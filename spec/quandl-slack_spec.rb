698e929b8a55eea6cff39cbd72684c8efd92e84f
Najwa Azer <najwa.azer@gmail.com>
2015-01-28 13:23:38 -0500

    Remove webhook URL

--- spec/quandl-slack_spec.rb
+++ spec/quandl-slack_spec.rb
@@ -12,7 +12,7 @@ describe Quandl::Slack do
 
   describe '.generate_notifier' do
     it 'generates Slack::Notifier instance' do
-      ENV['SLACK_WEBHOOK'] = 'https://hooks.slack.com/services/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'
+      ENV['SLACK_WEBHOOK'] = 'https://hooks.slack.com/services/fake/fake/fake'
       Quandl::Slack.generate_notifier('my_notifier', channel: '#stripe-testing', icon_emoji: ':christmas_tree:')
       expect(Quandl::Slack::MyNotifier.client).to be_kind_of(Slack::Notifier)
       expect(Quandl::Slack::MyNotifier.client.default_payload[:icon_emoji]).to eq(':christmas_tree:')
